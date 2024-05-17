dfd72bcba4f4b10b7d16c0c52d77d284746f65a3
HollowOFVoice <assasinstwo5@gmail.com>
2024-05-17 15:34:51 +0300

    Initial commit

---
+++ src/main/java/bip/online/isuus2024/Isuus2024Application.java
@@ -0,0 +1,13 @@
+package bip.online.isuus2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Isuus2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Isuus2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/bip/online/isuus2024/Isuus2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package bip.online.isuus2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Isuus2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
